f46e6d56fcb92094a8ad9527ec09ff8d7e7f0a51
Carlos Giovani Casilo <carlos.giovani@implantainformatica.com.br>
2020-02-03 14:15:46 -0300

    projeto mybooks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/views/login/login.component';
+import { HomeComponent } from './views/home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
